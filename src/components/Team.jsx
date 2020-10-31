4c337ddd70ffbe390ce259bb93982700547d3bc1
Vasu Goel <vasu18322@iiitd.ac.in>
2020-10-31 23:07:17 +0530

    Update Team.jsx
    Fixed minor margin and padding

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,12 +4,12 @@ import "./styles.css";
 import Teamcards from "./Teamcards";
 
 function Team(props){
-    return <div className="container pt-5">
-    <img style={{ borderRadius: "50%" }} src={props.image} width="255px" height="256px" />
+    return <div className="container pt-4 m-1">
+    <img style={{ borderRadius: "60%" }} src={props.image} width="240px" height="240px" />
     <div className="container text-center">
-   <a> <h2 className="pr-5" style={{color:"#828bb2",fontWeight:"bold"}}>{props.name}</h2> </a>
-   <p className="pr-5" style={{color:"#707B8E",fontSize:"19.2px"}}>{props.position}</p>
-   <p className="pr-5" style={{color:"#707B8E",fontSize:"16px",marginTop:"-10px"}}>{props.location}</p>
+   <a> <h3 className="pr-5" style={{color:"#828bb2",fontWeight:"bold"}}>{props.name}</h3> </a>
+   <p className="pr-5" style={{color:"#707B8E",fontSize:"19px"}}>{props.position}</p>
+   <p className="pr-5" style={{color:"#707B8E",fontSize:"16px",marginTop:"-12px"}}>{props.location}</p>
 
 </div>
     </div>

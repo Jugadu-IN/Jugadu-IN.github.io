144480bac0db87334c7317427b6e9d1e57495997
Vasu Goel <alohanvasu@gmail.com>
2020-10-31 23:05:38 +0530

    Update Landing.jsx
    Fixed padding and animate direcion

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,18 +14,18 @@ function Landing(){
 return <div className="container">
 
 <div className="row">
-    <div className="col-lg-6 ">
+    <div className="col-6">
     <h1  className="Heading w3-animate-left" >SASTA.SUNDAR.<br /> TECH-AO. </h1>   
     <div>
   <p className="content w3-animate-left">We at jugaadu bring your ideas to reality.Have a bussiness<br /> idea?Just contact us to make it a reality
 </p> 
 </div> 
 <div>
-<ReactBootStrap.Button className="w3-animate-left" style={{backgroundColor:"#c77b00",fontSize:"16px", padding: "30px 56px", lineHeight: "0",marginTop:"40px"}} size="lg" >Contact Us</ReactBootStrap.Button>{' '}
+<ReactBootStrap.Button className="w3-animate-left" style={{backgroundColor:"#c77b00",fontSize:"14px", padding: "30px 56px", lineHeight: "0",marginTop:"40px"}} size="lg" >Contact Us</ReactBootStrap.Button>{' '}
 </div>
     </div>
-   <div className="col-lg-6">
-  <img className="w3-animate-right" src={bgright} style={{height:"450px",paddingTop:"100px",paddingLeft:"30px"}} />
+   <div className="col-6">
+  <img className="w3-animate-left" src={bgright} style={{height:"450px",paddingTop:"100px",paddingLeft:"25px"}} />
    </div> 
 </div>
 </div>
